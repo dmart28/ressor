bcd4204b463ed44e2d7a62dcbfff9b731db825ae
Artem Dmitriev <art.dm.ser@gmail.com>
2019-09-30 17:28:20 +0300

    External API implementation finsihed

---
+++ ressor-commons/src/main/java/xyz/ressor/commons/exceptions/RessorBuilderException.java
@@ -0,0 +1,9 @@
+package xyz.ressor.commons.exceptions;
+
+public class RessorBuilderException extends RuntimeException {
+
+    public RessorBuilderException(String message) {
+        super(message);
+    }
+
+}
--- ressor-core/src/main/java/xyz/ressor/Ressor.java
+++ ressor-core/src/main/java/xyz/ressor/Ressor.java
@@ -2,11 +2,21 @@ package xyz.ressor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import xyz.ressor.commons.exceptions.RessorBuilderException;
+import xyz.ressor.commons.watch.fs.FileSystemWatchService;
+import xyz.ressor.ext.ServiceExtension;
+import xyz.ressor.service.RessorService;
+import xyz.ressor.service.proxy.ProxyContext;
+import xyz.ressor.service.proxy.ServiceProxyBuilder;
+import xyz.ressor.source.Source;
+import xyz.ressor.source.fs.FileSystemSource;
 import xyz.ressor.translator.Translator;
 import xyz.ressor.translator.Translators;
 
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.LinkedList;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -15,12 +25,25 @@ public class Ressor {
     private static final Logger log = LoggerFactory.getLogger(Ressor.class);
 
     public static <T> RessorBuilder<T> builder(Class<T> type) {
-        return new RessorBuilder<>();
+        return new RessorBuilder<>(type);
     }
 
     public static class RessorBuilder<T> {
+        private static final ServiceProxyBuilder proxyBuilder = new ServiceProxyBuilder();
+        private final Class<T> type;
         private Translator<InputStream, ?> translator;
         private Function<?, ? extends T> factory;
+        private Source source;
+        private FileSystemWatchService fsWatchService;
+        private T initialValue;
+        private boolean isAsync;
+        private ClassLoader classLoader;
+        private LinkedList<ServiceExtension> extensions = new LinkedList<>();
+        private Object[] proxyDefaultArguments;
+
+        public RessorBuilder(Class<T> type) {
+            this.type = type;
+        }
 
         public RessorBuilder<T> yaml() {
             this.translator = Translators.inputStream2Yaml();
@@ -66,8 +89,79 @@ public class Ressor {
             return this;
         }
 
+        public RessorBuilder<T> fileSource(String resourcePath) {
+            if (fsWatchService == null) {
+                fsWatchService = new FileSystemWatchService();
+            }
+            this.source = new FileSystemSource(resourcePath, fsWatchService);
+            return this;
+        }
+
+        public RessorBuilder<T> fileSource(FileSystemSource fileSource) {
+            this.source = fileSource;
+            return this;
+        }
+
+        public RessorBuilder<T> source(Source source) {
+            this.source = source;
+            return this;
+        }
+
+        public RessorBuilder<T> initialInstance(T initialValue) {
+            this.initialValue = initialValue;
+            return this;
+        }
+
+        public RessorBuilder<T> asyncInitialReload() {
+            this.isAsync = true;
+            return this;
+        }
+
+        public RessorBuilder<T> classLoader(ClassLoader classLoader) {
+            this.classLoader = classLoader;
+            return this;
+        }
+
+        public RessorBuilder<T> addExtension(ServiceExtension extension) {
+            this.extensions.add(extension);
+            return this;
+        }
+
+        public RessorBuilder<T> proxyDefaultArguments(Object... proxyDefaultArguments) {
+            this.proxyDefaultArguments = proxyDefaultArguments;
+            return this;
+        }
+
         public T build() {
-            return null;
+            if (source == null) {
+                throw new RessorBuilderException("No source instance provided");
+            }
+            if (translator == null) {
+                throw new RessorBuilderException("The data format of the source is unknown, please provide a translator");
+            }
+            if (fsWatchService != null) {
+                fsWatchService.init();
+            }
+            var ctx = ProxyContext.builder(type)
+                    .classLoader(classLoader)
+                    .factory(factory)
+                    .proxyDefaultArguments(proxyDefaultArguments)
+                    .initialInstance(initialValue)
+                    .translator(translator);
+            if (extensions.size() > 0) {
+                extensions.forEach(ctx::addExtension);
+            }
+            var proxy = (RessorService<T>) proxyBuilder.buildProxy(ctx.build());
+            if (isAsync) {
+                ForkJoinPool.commonPool().submit(() -> reload(proxy));
+            } else {
+                reload(proxy);
+            }
+            return (T) proxy;
+        }
+
+        private void reload(RessorService<T> proxy) {
+            proxy.reload(source.load());
         }
 
     }
